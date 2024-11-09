8e602b5e8254bafa42a90229a5eee5774d74317c
José Cristiano <cristiano.silva.devtech@gmail.com>
2024-11-09 00:03:15 -0300

    Criação da classe aula3 para teste

--- src/test/java/br/com/loiane/estruturadados/vetor/Aula03.java
+++ src/test/java/br/com/loiane/estruturadados/vetor/Aula03.java
@@ -1,2 +1,12 @@
-package br.com.loiane.estruturadados.vetor;public class Aula03 {
+package br.com.loiane.estruturadados.vetor;
+
+public class Aula03 {
+    public static void main(String[] args){
+
+        Vetor vetor = new Vetor(5);
+
+        vetor.adiciona("elemento 1");
+        vetor.adiciona("elemento 2");
+        vetor.adiciona("elemento 3");
+    }
 }
